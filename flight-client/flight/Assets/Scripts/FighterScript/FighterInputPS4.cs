7fa4d95b4abe6104759754f14c90cf41ab2e848e
kaitomishina <kai10mishina@weseek.co.jp>
2019-10-28 03:52:38 +0900

    create ace combat move

--- flight-client/flight/Assets/Scripts/FighterScript/FighterInputPS4.cs
+++ flight-client/flight/Assets/Scripts/FighterScript/FighterInputPS4.cs
@@ -33,7 +33,9 @@ namespace Fighter// 戦闘機周りはこの名前空間で統一
 
             bool isAutoPilot = false; // ヨーの左右同時入力時、オートパイロットをオンにする
             bool isPitchup = false; //ピッチアップしているときは、空力とか無視したい
-
+            if(pitch < 0) {
+                isPitchup = true;
+            }
             //float throttle = airBrakes ? -1 : 1;
             if (airEngines == true)
             {
@@ -46,23 +48,27 @@ namespace Fighter// 戦闘機周りはこの名前空間で統一
 
             // Pass the input to the aeroplane
             //現状PS4コントローラにのみ対応!
+            // TODO いつか改善したい
             if (yaw1 < 0 && yaw2 < 0)
             {
                 m_Aeroplane.Move(roll, pitch, 0, throttle, airBrakes);
+                m_Aeroplane.SetFighterStatus(isAutoPilot, isPitchup);
             }
             if (yaw1 >= 0 && yaw2 < 0)
             {
                 m_Aeroplane.Move(roll, pitch, -yaw1, throttle, airBrakes);
+                m_Aeroplane.SetFighterStatus(isAutoPilot, isPitchup);
             }
             if (yaw2 >= 0 && yaw1 < 0)
             {
                 m_Aeroplane.Move(roll, pitch, yaw2, throttle, airBrakes);
+                m_Aeroplane.SetFighterStatus(isAutoPilot, isPitchup);
             }
             if (yaw1 >= 0 && yaw2 >= 0)
             {
                 isAutoPilot = true; 
                 m_Aeroplane.Move(roll, pitch, 0, throttle, airBrakes);
-                m_Aeroplane.SetFighterStatus(isAutoPilot);
+                m_Aeroplane.SetFighterStatus(isAutoPilot, isPitchup);
             }
 
 
