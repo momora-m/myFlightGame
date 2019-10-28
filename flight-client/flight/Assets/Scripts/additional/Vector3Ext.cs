07ba3e07ee826bf7f31d627e9a9c3bd7831f022f
kaitomishina <kai10mishina@weseek.co.jp>
2019-10-28 17:55:04 +0900

    edit-camera

---
+++ flight-client/flight/Assets/Scripts/additional/Vector3Ext.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+
+public static class Vector3Ext
+{
+    public static Vector3 Round(this Vector3 self)
+    {
+        return new Vector3
+        (
+            Mathf.Round(self.x),
+            Mathf.Round(self.y),
+            Mathf.Round(self.z)
+        );
+    }
+}
